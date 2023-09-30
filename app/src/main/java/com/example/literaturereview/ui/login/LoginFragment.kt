fcb53ddcdb97f7199c112f73dd6fefd27c42d8e5
Omolara Idowu <omolaraidowu44@gmail.com>
2023-09-30 12:18:07 +0100

    - Update UI for Login and Sign up screens

--- app/src/main/java/com/example/literaturereview/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/literaturereview/ui/login/LoginFragment.kt
@@ -6,15 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.literaturereview.R
+import androidx.navigation.fragment.findNavController
+import com.example.literaturereview.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_promptFragment)
+        }
+
+        binding.signupTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+
+        binding.forgotPasswordTextView.setOnClickListener {
+            // navigate to forgot password screen, could be a bottom sheet view
+        }
+
+        binding.googleLoginButton.setOnClickListener {
+
+        }
     }
 
 }
