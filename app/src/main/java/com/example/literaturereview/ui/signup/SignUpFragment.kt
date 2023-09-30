f14735914fd700c05204337d2d49a85563e4b823
Omolara Idowu <omolaraidowu44@gmail.com>
2023-09-30 12:18:07 +0100

    - Update UI for Login and Sign up screens

--- app/src/main/java/com/example/literaturereview/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/literaturereview/ui/signup/SignUpFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.literaturereview.R
+import com.example.literaturereview.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createAccountButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+
+        binding.termsText.setOnClickListener {
+            // navigates to terms screen, could be a dialog
+        }
+
+        binding.loginTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+
+        binding.googleSignupButton.setOnClickListener {
+
+        }
     }
 }
