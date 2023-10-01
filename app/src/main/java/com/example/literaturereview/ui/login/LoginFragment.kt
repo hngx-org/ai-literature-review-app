aa94329d3b7e175e64b37375793fbadeeaf225ee
Omolara Idowu <omolaraidowu44@gmail.com>
2023-10-01 03:12:48 +0100

    - Add validation for the name field - Add email validation to handle empty input and valid email formats - Add password validation - Update the login button enabling logic to consider all fields

--- app/src/main/java/com/example/literaturereview/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/literaturereview/ui/login/LoginFragment.kt
@@ -1,12 +1,14 @@
 package com.example.literaturereview.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.literaturereview.R
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.literaturereview.R
 import com.example.literaturereview.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -25,20 +27,79 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.loginButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_promptFragment)
-        }
+        setupClickListeners()
+        setupTextChangeListeners()
+    }
 
+    private fun setupClickListeners() {
         binding.signupTextView.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
         }
 
         binding.forgotPasswordTextView.setOnClickListener {
-            // navigate to forgot password screen, could be a bottom sheet view
+            // Navigate to the forgot password screen (e.g., using a bottom sheet)
         }
 
         binding.googleLoginButton.setOnClickListener {
+            // Implement Google Sign-In functionality
+        }
 
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_promptFragment)
+        }
+    }
+
+    private fun setupTextChangeListeners() {
+        binding.emailEditText.doOnTextChanged { _, _, _, _ ->
+            binding.emailInputLayout.error = validEmail()
+            validateButton()
+        }
+
+        binding.passwordEditText.doOnTextChanged { _, _, _, _ ->
+            binding.passwordInputLayout.error = validPassword()
+            validateButton()
+        }
+    }
+
+    private fun validEmail(): String? {
+        val email = binding.emailEditText.text.toString()
+
+        return when {
+            email.isBlank() -> null
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> "Please enter a valid email address"
+            else -> null
+        }
+    }
+
+    private fun validPassword(): String? {
+        val password = binding.passwordEditText.text.toString()
+
+        val containsLowercase = password.any { it.isLowerCase() }
+        val containsUppercase = password.any { it.isUpperCase() }
+        val containsDigit = password.any { it.isDigit() }
+        val containsSpecialCharacter = password.any { !it.isLetterOrDigit() }
+
+        return when {
+            password.isBlank() -> "Password cannot be empty"
+            password.length < 8 -> "Password must be at least 8 characters long"
+            !containsLowercase -> "Password must contain at least one lowercase letter"
+            !containsUppercase -> "Password must contain at least one uppercase letter"
+            !containsDigit -> "Password must contain at least one digit"
+            !containsSpecialCharacter -> "Password must contain at least one special character"
+            else -> null
+        }
+    }
+
+    private fun validateButton() {
+        val validEmail = validEmail() == null
+        val validPassword = validPassword() == null
+
+        if (validEmail && validPassword) {
+            binding.loginButton.alpha = 1f
+            binding.loginButton.isEnabled = true
+        } else {
+            binding.loginButton.alpha = 0.5f
+            binding.loginButton.isEnabled = false
         }
     }
 
--- app/src/main/java/com/example/literaturereview/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/literaturereview/ui/signup/SignUpFragment.kt
@@ -1,10 +1,12 @@
 package com.example.literaturereview.ui.signup
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.literaturereview.R
 import com.example.literaturereview.databinding.FragmentSignUpBinding
@@ -25,6 +27,11 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpClickListeners()
+        setUpTextChangeListeners()
+    }
+
+    private fun setUpClickListeners() {
         binding.createAccountButton.setOnClickListener {
             findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
         }
@@ -39,6 +46,74 @@ class SignUpFragment : Fragment() {
 
         binding.googleSignupButton.setOnClickListener {
 
+        }
+    }
+
+    private fun setUpTextChangeListeners() {
+
+        binding.nameEditText.doOnTextChanged { _, _, _, _ ->
+            binding.nameInputLayout.error = validName()
+        }
+
+        binding.emailEditText.doOnTextChanged { _, _, _, _ ->
+            binding.emailInputLayout.error = validEmail()
+            validateButton()
+        }
+
+        binding.passwordEditText.doOnTextChanged { _, _, _, _ ->
+            binding.passwordInputLayout.error = validPassword()
+            validateButton()
+        }
+    }
+
+    private fun validName(): String? {
+        val name = binding.nameEditText.text.toString()
+        return when {
+            name.isBlank() -> "Name cannot be empty"
+            else -> null
+        }
+    }
+
+    private fun validEmail(): String? {
+        val email = binding.emailEditText.text.toString()
+
+        return when {
+            email.isBlank() -> null
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> "Please enter a valid email address"
+            else -> null
+        }
+    }
+
+    private fun validPassword(): String? {
+        val password = binding.passwordEditText.text.toString()
+
+        val containsLowercase = password.any { it.isLowerCase() }
+        val containsUppercase = password.any { it.isUpperCase() }
+        val containsDigit = password.any { it.isDigit() }
+        val containsSpecialCharacter = password.any { !it.isLetterOrDigit() }
+
+        return when {
+            password.isBlank() -> "Password cannot be empty"
+            password.length < 8 -> "Password must be at least 8 characters long"
+            !containsLowercase -> "Password must contain at least one lowercase letter"
+            !containsUppercase -> "Password must contain at least one uppercase letter"
+            !containsDigit -> "Password must contain at least one digit"
+            !containsSpecialCharacter -> "Password must contain at least one special character"
+            else -> null
+        }
+    }
+
+    private fun validateButton() {
+        val validName = validName() == null
+        val validEmail = validEmail() == null
+        val validPassword = validPassword() == null
+
+        if (validName && validEmail && validPassword) {
+            binding.createAccountButton.alpha = 1f
+            binding.createAccountButton.isEnabled = true
+        } else {
+            binding.createAccountButton.alpha = 0.5f
+            binding.createAccountButton.isEnabled = false
         }
     }
 }
